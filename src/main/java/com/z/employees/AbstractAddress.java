518c7f8516b0383bca6d5d0f0318eb85268a8a0a
ryan ☆ <ryan@ryans-MacBook-Pro.local>
2024-10-31 16:16:05 -0400

    implemented code in abstract address and address classes

--- src/main/java/com/z/employees/AbstractAddress.java
+++ src/main/java/com/z/employees/AbstractAddress.java
@@ -1,5 +1,51 @@
 package com.z.employees;
 
-public abstract class AbstractAddress {
-    
+public abstract class AbstractAddress 
+{
+    public String street, city, state;
+    public int zipCode; 
+
+    public AbstractAddress(String _street, String _city, String _state, int _zipCode)
+    {
+        street = _street;
+        city = _city;
+        state = _state;
+        zipCode = _zipCode;
+    }
+
+    public String getStreet()
+    {
+        return street;
+    }
+    public void setStreet(String _street)
+    {
+        street = _street;
+    }
+
+    public String getCity()
+    {
+        return city;
+    }
+    public void setCity(String _city)
+    {
+        city = _city;
+    }
+
+    public String getState()
+    {
+        return state;
+    }
+    public void setState(String _state)
+    {
+        state = _state
+    }
+
+    public int getZipCode()
+    {
+        return zipCode;
+    }
+    public void setZipCode(int _zipCode)
+    {
+        zipCode = _zipCode;
+    }
 }
--- src/main/java/com/z/employees/Address.java
+++ src/main/java/com/z/employees/Address.java
@@ -2,5 +2,8 @@ package com.z.employees;
 
 public class Address extends AbstractAddress
 {
-    
+    public Address(String street, String city, String state, int zipCode)
+    {
+        super(street, city, state, zipCode);
+    }
 }
