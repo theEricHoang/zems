56005aea35e2c2aa0f0d35f8ae928a6cc98ab43b
ryancondones <ryannjr2004@gmail.com>
2024-11-10 14:15:37 -0500

    Implemented getters and setters in Payroll classes and additional fields in Address classes to contain other database values

--- src/main/java/com/z/model/IPayroll.java
+++ src/main/java/com/z/model/IPayroll.java
@@ -1,5 +1,30 @@
 package com.z.model;
 
 public interface IPayroll {
-    void generatePayStatement();
+    public int getEmpID();
+    public void setEmpID(int _empID);
+
+    public int getPayDate();
+    public void setPayDate(int _payDate);
+
+    public int getGross();
+    public void setGross(int _gross);
+
+    public int getFederal();
+    public void setFederal(int _federal);
+
+    public int getFedMed();
+    public void setFedMed(int _fedMed);
+
+    public int getFedSS();
+    public void setFedSS(int _fedSS);
+
+    public int getState();
+    public void setState(int _state);
+
+    public int get401K();
+    public void set401K(int _emp401K);
+
+    public int getHealthCare();
+    public void setHealthCare(int _healthCare);
 }
